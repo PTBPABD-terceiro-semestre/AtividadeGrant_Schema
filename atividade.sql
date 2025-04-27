42324b7c0149aa5f01094d961a8b9c26030a9153
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-04-27 02:56:58 -0300

    Update atividade.sql

--- atividade.sql
+++ atividade.sql
@@ -2,11 +2,11 @@
 Questão 01. Crie as contas de usuário User_A, User_B, User_C, User_D e User_E.
 */
 
-create user User_A with password = '1234a';
-create user User_B with password = '1234b';
-create user User_C with password = '1234c';
-create user User_D with password = '1234d';
-create user User_E with password = '1234e';
+create user User_A with password = '1234567a';
+create user User_B with password = '1234567b';
+create user User_C with password = '1234567c';
+create user User_D with password = '1234567d';
+create user User_E with password = '1234567e';
 
 /*
 Questão 02. Considere o esquema de banco de dados relacional university.
