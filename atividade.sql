8432e3c229c44aec46e8e34d8e5bb8397814750c
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-05-07 18:53:04 -0300

    Update atividade.sql

--- atividade.sql
+++ atividade.sql
@@ -2,11 +2,11 @@
 Questão 01. Crie as contas de usuário User_A, User_B, User_C, User_D e User_E.
 */
 
-create user User_A with password = '1234567a';
-create user User_B with password = '1234567b';
-create user User_C with password = '1234567c';
-create user User_D with password = '1234567d';
-create user User_E with password = '1234567e';
+create user User_A with password = '1234567@A';
+create user User_B with password = '1234567@B';
+create user User_C with password = '1234567@C';
+create user User_D with password = '1234567@D';
+create user User_E with password = '1234567@E';
 
 /*
 Questão 02. Considere o esquema de banco de dados relacional university.
@@ -14,11 +14,21 @@ O User_A poderá selecionar ou modificar qualquer relação, exceto CLASSROOM,
 e pode conceder qualquer um desses privilégios a outros usuários.
 */
 
-create schema university;
-grant select, alter on ? to User_A with grant option;
+grant select, alter on takes to User_A with grant option;
+grant select, alter on advisor to User_A with grant option;
+grant select, alter on course to User_A with grant option;
+grant select, alter on department to User_A with grant option;
+grant select, alter on endereco to User_A with grant option;
+grant select, alter on instructor to User_A with grant option;
+grant select, alter on pessoa to User_A with grant option;
+grant select, alter on prereq to User_A with grant option;
+grant select, alter on section to User_A with grant option;
+grant select, alter on student to User_A with grant option;
+grant select, alter on teaches to User_A with grant option;
+grant select, alter on time_slot to User_A with grant option;
 
 /*
 Questão 03. Liste as permissões do User_A.
 */
 
-select university. ;
+select ;
